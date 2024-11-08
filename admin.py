095107785a47168109ed4bc2e7646d2b23dbc157
Joe <joe@joeslat.tail554170.ts.net>
2024-11-08 13:45:49 -0500

    11/8/24

--- admin.py
+++ admin.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 from functools import wraps
-from utils import calculate_log_summary
 
 admin_bp = Blueprint('admin', __name__)
 
@@ -11,12 +10,6 @@ def admin_required(f):
             return redirect(url_for('auth.login'))
         return f(*args, **kwargs)
     return decorated_function
-
-@admin_bp.route('/view_logs')
-@admin_required
-def view_logs():
-    log_summary = calculate_log_summary()
-    return render_template('view_logs.html', summary=log_summary)
 
 @admin_bp.route('/system-monitor')
 @admin_required
